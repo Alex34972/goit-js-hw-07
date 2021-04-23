0c06751c1560004fbfd295f52d16eacc001d34b6
Alexandr Petrenko <74877390+Alex34972@users.noreply.github.com>
2021-04-23 22:05:40 +0300

    Delete src directory

--- src/js/task-05.js
+++
@@ -1,6 +0,0 @@
-const inputEl = document.querySelector("#name-input");
-const spanEl = document.querySelector("#name-output");
-inputEl.addEventListener(`input`, insertTextWithInput);
-function insertTextWithInput() {
-  spanEl.textContent = inputEl.value;
-}
--- src/js/task-07.js
+++
@@ -1,6 +0,0 @@
-const inputEl = document.querySelector("#font-size-control");
-const spanEl = document.querySelector("#text");
-inputEl.addEventListener(`change`, removeSizeText);
-function removeSizeText(event) {
-  spanEl.style.fontSize = `${event.currentTarget.value}.px`;
-}
