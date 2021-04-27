873ecf3dfbb83636f5e5f67a30b849b355165b53
Alexandr Petrenko <74877390+Alex34972@users.noreply.github.com>
2021-04-27 08:40:59 +0300

    Add files via upload

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,7 @@
 const inputEl = document.querySelector("#name-input");
 const spanEl = document.querySelector("#name-output");
 inputEl.addEventListener(`input`, insertTextWithInput);
-const textGreeting = `незнакомец`;
+const textGreeting = spanEl.textContent;
 function insertTextWithInput() {
   if (inputEl.value !== ``) {
     return (spanEl.textContent = inputEl.value);
